ff62f09c02cf5088b5d987dd9e49f03d98a2d99f
Aluno <Aluno@Digital.net>
2019-06-03 18:31:20 -0300

    Atualizações do Site

--- NuvemLiteraria/routes/usuarios.js
+++ NuvemLiteraria/routes/usuarios.js
@@ -13,7 +13,7 @@ router.post('/entrar', function (req, res, next) {
     if (login == undefined || senha == undefined) {
       throw new Error(`Dados de login não chegaram completos: ${login} / ${senha}`);
     }
-    return banco.sql.query(`select * from usuario where Email='${login}' and Senha='${senha}'`);
+    return banco.sql.query(`select * from DadoPessoal where Email='${login}' and Senha='${senha}'`);
   }).then(consulta => {
 
     console.log(`Usuários encontrados: ${JSON.stringify(consulta.recordset)}`);
@@ -46,14 +46,15 @@ router.post('/cadastrar', function (req, res, next) {
 
   banco.conectar().then(() => {
     console.log(`Chegou p/ cadastro: ${JSON.stringify(req.body)}`);
-	nome = req.body.nome; // depois de .body, use o nome (name) do campo em seu formulário de login
-    login = req.body.login; // depois de .body, use o nome (name) do campo em seu formulário de login
+	nome = req.body.nomec; // depois de .body, use o nome (name) do campo em seu formulário de login
+    login = req.body.email; // depois de .body, use o nome (name) do campo em seu formulário de login
     senha = req.body.senha; // depois de .body, use o nome (name) do campo em seu formulário de login
+ 
     if (login == undefined || senha == undefined || nome == undefined) {
 	  // coloque a frase de erro que quiser aqui. Ela vai aparecer no formulário de cadastro
       throw new Error(`Dados de cadastro não chegaram completos: ${login} / ${senha} / ${nome}`);
     }
-    return banco.sql.query(`select count(*) as contagem from usuario where login = '${login}'`);
+    return banco.sql.query(`select count(*) as contagem from DadoPessoal where email = '${login}'`);
   }).then(consulta => {
 
 	if (consulta.recordset[0].contagem >= 1) {
@@ -73,7 +74,7 @@ router.post('/cadastrar', function (req, res, next) {
   }).finally(() => {
 	  if (cadastro_valido) {		  
 			  
-		banco.sql.query(`insert into usuario (nome, login, senha) values ('${nome}','${login}','${senha}')`).then(function() {
+		banco.sql.query(`insert into DadoPessoal (Nome, Email, Senha) values ('${nome}','${login}','${senha}')`).then(function() {
 			console.log(`Cadastro criado com sucesso!`);
 			res.sendStatus(201); 
 			// status 201 significa que algo foi criado no back-end, 
